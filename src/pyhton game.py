b559f731e9d2b169c7aee6c9246cf31a190665e7
21074 <21074@bridgend.ac.uk>
2022-03-04 13:12:45 +0000

    saving

--- src/pyhton game.py
+++ src/pyhton game.py
@@ -1,7 +1,23 @@
+from itertools import count
+
+
 def main():
     print("Welcome to my game")
 
-    player_name=input("what is your name?" )
-    print("Hi" + player_name)
+    player_name=input("what is your name? ")
+    print("Hi " + player_name)
+
+    lives = 3
+    print(f"You have {lives} lives remaining")
+
+
+
+
+
+
+
+
+
+
 
 main()
