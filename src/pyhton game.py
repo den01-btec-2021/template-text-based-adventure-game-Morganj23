7a1d170a7f4695f947015e63f3cda98e3fc11bb5
21074 <21074@bridgend.ac.uk>
2022-03-04 13:27:01 +0000

    direction for player to move

--- src/pyhton game.py
+++ src/pyhton game.py
@@ -10,6 +10,24 @@ def main():
     lives = 3
     print(f"You have {lives} lives remaining")
 
+    print("you can move North,South,East,West")
+    direction =input("Which direction do you want to go? ")
+   
+    if direction == "North":
+        print("You went North")
+    
+    elif direction == "South":
+        print("You went South")
+    
+    elif direction == "East":
+        print("You went East")
+   
+    else:
+        print("You went West")
+
+
+
+
 
 
 
