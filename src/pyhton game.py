d5af5c4fa9bc75227d38c062dacb48012f5fc29e
21074 <21074@bridgend.ac.uk>
2022-03-11 14:44:10 +0000

    adding prints

--- src/pyhton game.py
+++ src/pyhton game.py
@@ -1,3 +1,6 @@
+from ast import Return, While
+
+
 def main():
     print("Welcome to my game")
 
@@ -8,28 +11,31 @@ def main():
     lives = 3
     print(f"You have {lives} lives remaining, make them count!")
     print("You are located in an abandoned laboratory and you must find away to get out" )
-    print("To successfully leave the building you must enter 4 other rooms in which you will enntouer puzzles which will be rquired towards the end of the game" )
+    print("To successfully leave the building you must enter 4 other rooms in which you will encounter puzzles which will be rquired towards the end of the game" )
     
-
     print("You notice a door ahead of you which looks to be pried open")
 
-    print("To move towards the enter North")
+    direction =input("To contiunue North press Enter")
     
+    print("you enter a long dark corridor,you notice there are 4 doors numbered 1,2,3 and 4")
+    print("Door1 = North, Door2 = South, Door3 = East, Door4 = West")
+
     
     while True:
         direction =input("Which direction do you want to go? ")
         
         if direction == "North":
             print("You went North")
-            puzzle_answer_North = input("What is 2+2? ")
-            if puzzle_answer_North == "4":
+            puzzle_answer_North = input("6*6? ")
+            if puzzle_answer_North == "36":
                 print("Correct")
-                backpack.append("Key 1")
+                backpack.append("Blue screwdriver")
 
             else:
                 print("Incorrect")
                 lives -= 1
                 print(f"You have {lives} lives remaining ")
+               
         
         
         
@@ -51,7 +57,7 @@ def main():
 
 
         #if backpack is full, open door, win game  
-        if ("Key 1"in backpack) and ("Key 2"in backpack) and ("Key 3"in backpack) and ("Key 4"in backpack):
+        if ("Blue screwdriver"in backpack) and ("Red screwdriver"in backpack) and ("Black screwdriver"in backpack) and ("Yellow screwdriver"in backpack):
             print("Vents open!, you got out")
         
         #when lives reach 0, game ends
