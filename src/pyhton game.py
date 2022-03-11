5e88ac360b4404cd6164861121ef027f646a55ed
21074 <21074@bridgend.ac.uk>
2022-03-11 12:29:43 +0000

    started rooms, collect key

--- src/pyhton game.py
+++ src/pyhton game.py
@@ -1,12 +1,10 @@
-from itertools import count
-
-
 def main():
     print("Welcome to my game")
 
     player_name=input("what is your name? ")
     print("Hi " + player_name)
 
+    backpack = [] #initialsise empty list for backpack
     lives = 3
     print(f"You have {lives} lives remaining, make them count!")
     print("You are located in an abandoned laboratory and you must find away to get out" )
@@ -16,22 +14,50 @@ def main():
     print("You notice a door ahead of you which looks to be pried open")
 
     print("To move towards the enter North")
-    direction =input("Which direction do you want to go? ")
-   
-    if direction == "North":
-        print("You went North")
     
-    elif direction == "South":
-        print("You went South")
     
-    elif direction == "East":
-        print("You went East")
-    
-    elif direction == "West":
-        print("You went West")
+    while True:
+        direction =input("Which direction do you want to go? ")
+        
+        if direction == "North":
+            print("You went North")
+            puzzle_answer_North = input("What is 2+2? ")
+            if puzzle_answer_North == "4":
+                print("Correct")
+                backpack.append("Key 1")
 
-    else:
-        print("Sorry,not recognised")        
+            else:
+                print("Incorrect")
+                lives -= 1
+                print(f"You have {lives} lives remaining ")
+        
+        
+        
+        
+        
+        
+        
+        elif direction == "South":
+            print("You went South")
+        
+        elif direction == "East":
+            print("You went East")
+        
+        elif direction == "West":
+            print("You went West")
+
+        else:
+            print("Sorry,not recognised") 
+
+
+        #if backpack is full, open door, win game  
+        if ("Key 1"in backpack) and ("Key 2"in backpack) and ("Key 3"in backpack) and ("Key 4"in backpack):
+            print("Vents open!, you got out")
+        
+        #when lives reach 0, game ends
+        if lives == 0:
+            print("You perished")  
+            exit()   
 
 
 
